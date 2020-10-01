b05018419276473d453b4ce17ae75fc74ae8621c
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-10-01 22:42:59 +0300

    Error correction

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -3,9 +3,13 @@ package ru.netology;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -15,6 +19,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class CallBackTest {
 
     private Faker faker;
+
     @BeforeEach
     void setUpAll() {
         faker = new Faker(new Locale("ru"));
@@ -26,16 +31,42 @@ public class CallBackTest {
 
         RegistrationByCardInfo info = new RegistrationByCardInfo();
 
+        $("[data-test-id='city'] input").setValue(faker.address().cityName());
+
+//        Очищаем поле ввода даты
+        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+//        Генерируем строку с датой
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//        Ввод сгенерированной даты
+        $("[data-test-id=date] input.input__control").setValue(date);
+
+        $("[data-test-id='name'] input").setValue(faker.name().fullName());
+
+        $("[data-test-id='phone'] input").setValue(faker.phoneNumber().phoneNumber());
+
+        $("[data-test-id=agreement]").click();
 
         $(byText("Запланировать")).click();
 
         $(withText("Успешно!")).waitUntil(visible, 15000);
 
-        $(byText("Перепланировать")).click();
-        $(withText("Успешно!")).waitUntil(visible, 15000);
 //      Проверка даты в сплывающем сообщении
-//        $("[data-test-id=notification] .notification__content").waitUntil(visible, 15000).shouldHave(text(date));
+        $("[data-test-id='success-notification'] .notification__content").waitUntil(visible, 15000).shouldHave(text(date));
 
+//        Очищаем поле ввода даты
+        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+//        Генерируем строку с датой
+        String date2 = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//        Ввод сгенерированной даты
+        $("[data-test-id=date] input.input__control").setValue(date);
 
+        $(byText("Запланировать")).click();
+
+        $(byText("Перепланировать")).click();
+
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+
+//        Проверка даты в сплывающем сообщении
+//        $("[data-test-id='success-notification'] .notification__content").waitUntil(visible, 15000).shouldHave(text(date2));
     }
-    }
+}
--- src/test/java/ru/netology/RegistrationByCardInfo.java
+++ src/test/java/ru/netology/RegistrationByCardInfo.java
@@ -1,12 +1,9 @@
 package ru.netology;
 
 import com.github.javafaker.Faker;
-import com.github.javafaker.PhoneNumber;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
     @Data
     @RequiredArgsConstructor
@@ -15,7 +12,7 @@ import java.time.format.DateTimeFormatter;
         Faker faker = new Faker();
 
         private final String address = faker.address().cityName();
-        private final String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//        private final String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         private final String name = faker.name().fullName();
-        private final PhoneNumber phone = faker.phoneNumber();
+        private final String phone = faker.phoneNumber().phoneNumber();
 }
