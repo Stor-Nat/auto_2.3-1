1b0230faeb7c0a0f1a1821fdae9475415f74b183
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-10-07 09:52:16 +0300

    Bugs fixed

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -3,34 +3,31 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.*;
 
 public class CallBackTest {
 
     @Test
     void shouldSubmitRequest() {
+        String planMeetingDate = getDate(3);
+        String replanMeetingDate = getDate(7);
         open("http://localhost:9999");
 
-        $("[data-test-id='city'] input").setValue(RegistrationByCardInfo.city());
+        $("[data-test-id='city'] input").setValue(city());
 
 //        Очищаем поле ввода даты
         $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-//        Генерируем строку с датой
-        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//        Ввод сгенерированной даты
-        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id=date] input").setValue(planMeetingDate);
 
-        $("[data-test-id='name'] input").setValue(RegistrationByCardInfo.name());
+        $("[data-test-id='name'] input").setValue(name());
 
-        $("[data-test-id='phone'] input").setValue(RegistrationByCardInfo.phone());
+        $("[data-test-id='phone'] input").setValue(phone());
 
         $("[data-test-id=agreement]").click();
 
@@ -39,14 +36,11 @@ public class CallBackTest {
         $(withText("Успешно!")).waitUntil(visible, 15000);
 
 //      Проверка даты в сплывающем сообщении
-        $("[data-test-id='success-notification'] .notification__content").waitUntil(visible, 15000).shouldHave(text(date));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(text("Встреча успешно запланирована на " + planMeetingDate));
 
 //        Очищаем поле ввода даты
         $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
-//        Генерируем строку с датой
-        String date2 = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//        Ввод сгенерированной даты
-        $("[data-test-id=date] input.input__control").setValue(date);
+        $("[data-test-id=date] input.input__control").setValue(replanMeetingDate);
 
         $(byText("Запланировать")).click();
 
@@ -55,6 +49,6 @@ public class CallBackTest {
         $(withText("Успешно!")).waitUntil(visible, 15000);
 
 //        Проверка даты в сплывающем сообщении
-//        $("[data-test-id='success-notification'] .notification__content").waitUntil(visible, 15000).shouldHave(text(date2));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(text("Встреча успешно запланирована на " + replanMeetingDate));
     }
 }
